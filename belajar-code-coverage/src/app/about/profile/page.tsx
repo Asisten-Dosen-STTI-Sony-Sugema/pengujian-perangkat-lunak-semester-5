520df356d2f4d1a96f4698b1995228840b4fd367
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-12-02 11:05:17 +0700

    add learn code coverage

---
+++ belajar-code-coverage/src/app/about/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function ProPage() {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+    </div>
+  );
+}
---
+++ belajar-code-coverage/src/app/api/product/route.ts
@@ -0,0 +1,58 @@
+import { retrieveData, retrieveDataById } from "@/lib/firebase/service";
+import { NextRequest, NextResponse } from "next/server";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const data = [
+  {
+    id: 1,
+    title: "Nike Pegasus 41 Blueprint",
+    price: 2099000,
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4615e080-c7a7-470e-b96a-02370aba3276/AIR+ZOOM+PEGASUS+41+FP.png",
+  },
+  {
+    id: 2,
+    title: "Nike Mercurial Superfly 10 Academy",
+    price: 1499000,
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a10e080b-1985-48dc-83d5-ce0d86a294b7/ZM+SUPERFLY+10+ACADEMY+TF.png",
+  },
+  {
+    id: 3,
+    title: "Giannis Freak 6 EP",
+    price: 2199000,
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/cad08379-10fd-45a9-98db-5689453f8353/GIANNIS+FREAK+6+NRG+EP.png",
+  },
+  {
+    id: 4,
+    title: "Giannis Freak 6 EP",
+    price: 2199000,
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/cad08379-10fd-45a9-98db-5689453f8353/GIANNIS+FREAK+6+NRG+EP.png",
+  },
+];
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
+  if (id) {
+    const detailProduct = await retrieveDataById("products", id);
+    if (detailProduct) {
+      return NextResponse.json({
+        status: 200,
+        message: "Success",
+        data: detailProduct,
+      });
+    }
+    return NextResponse.json({
+      status: 404,
+      message: "Not Found",
+      data: {},
+    });
+  }
+
+  const products = await retrieveData("products");
+
+  return NextResponse.json({ status: 200, message: "Success", data: products });
+}
